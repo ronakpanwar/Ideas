e8f6914376cd4905dae9ffcb7c70aa70c4357a35
ronakpanwar <ronakpanwar18@gmail.com>
2025-09-02 16:44:53 +0530

    change

--- client/src/components/PostCards.tsx
+++ client/src/components/PostCards.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from './ui/card'
-import Image from 'next/image'
-import temp from './temp.jpg'
+// import Image from 'next/image'
+// import temp from './temp.jpg'
 import { Button } from './ui/button'
 import { useRouter } from 'next/navigation'
+// import { Badge } from './ui/badge'
 
 const PostCard = ({post}) => {
 
@@ -13,22 +14,24 @@ const PostCard = ({post}) => {
     <div>
       <Card className='flex flex-col items-center gap-2 shadow-lg hover:shadow-xl ' >
         <CardHeader className='rounded-t-lg '>
-          <Image
+          {/* <Image
           className='w-full rounded-t-md object-cover h-auto'
            width={500}
             height={200}
             src={ post?.img ? post.img :temp}
-            alt='postImage' />
+            alt='postImage' /> */}
         </CardHeader>
         <CardContent className='flex flex-col gap-4 '>
-           <CardTitle>{post?.title}</CardTitle>
+           <CardTitle>{post?.title.substring(0,40)}...</CardTitle>
            <CardDescription>{post?.description.substring(0,200)}...</CardDescription>
          
         </CardContent>
-        <CardFooter className=''>
-            <Button onClick={()=>{
+        <CardFooter className=' flex '>
+            <Button className='text-sm' variant={"outline"} onClick={()=>{
              router.push(`/about/${post._id}`)
-            }}>Show More</Button>
+            }}>See More</Button>
+           
+          
         </CardFooter>
       </Card>
     </div>
