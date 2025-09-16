4a61a3071b4368a6f2e3b656d63c5873009ec3e1
ronakpanwar <ronakpanwar18@gmail.com>
2025-09-16 13:27:29 +0530

    change in config file

--- client/next.config.ts
+++ client/next.config.ts
@@ -4,6 +4,9 @@ const nextConfig: NextConfig = {
   images: {
     domains: ['res.cloudinary.com'], // Add the Cloudinary domain here
   },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
   // You can add any other Next.js configuration options here
 };
 
